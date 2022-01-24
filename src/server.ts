dfaf180915d32e1ca8e5c533dcdf8673b01f5559
Kauanphbbb <kauanphbbb@gmail.com>
2022-01-24 09:42:52 -0300

    added eslint airbnb

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,7 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
-});
+app.get('/', (req, res) => res.json({ message: 'Hello World' }));
 
-app.listen(3333, () => {
-  console.log("Server started on port 3333");
-});
+app.listen(3333, () => console.log('Server started on port 3333'));
